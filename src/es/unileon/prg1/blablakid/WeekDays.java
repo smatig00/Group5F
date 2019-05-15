973435059114c58a7c764912f3d9b40c1fd749d8
smatig00 <36730109+smatig00@users.noreply.github.com>
2019-05-15 04:13:33 +0200

    Update and rename Week.java to WeekDays.java

---
+++ src/es/unileon/prg1/blablakid/WeekDays.java
@@ -0,0 +1,22 @@
+package es.unileon.prg1.blablakid;
+
+/**
+ * Class that represents the week interface.
+ * 
+ * @author Team 5F.
+ * @version 1.0
+ *
+ */
+
+public class WeekDays {
+
+	private Day[] days;
+	
+	public WeekDays(int maxRides) {
+		this.days = new Day[5];
+		//TODO crear los dias de la semana
+		for(int i = 0; i < 5; i++) {
+			days[i] = 
+		}
+	}
+}
