372f57de7e90e9f012cf4500cd2f95b30d04b78e
smatig00 <36730109+smatig00@users.noreply.github.com>
2019-05-16 07:48:05 +0200

    Update Parents.java

--- src/es/unileon/prg1/blablakid/Parents.java
+++ src/es/unileon/prg1/blablakid/Parents.java
@@ -20,10 +20,9 @@ public class Parents {
 	static final Logger log = LogManager.getLogger(MainBlablakid.class);
 	
 	/**
-	 * Counter identifier for parents.
 	 * 
 	 */
-	private int counter;
+	private int next;
 	
 	/**
 	 * Parent type array identifier. Array where parents are saved.
@@ -32,7 +31,7 @@ public class Parents {
 	private Parent[] parents;
 	
 	/**
-	 * Builder of the class. Create a parent with name and a value.
+	 * Builder of the class.
 	 * 
 	 * @param maxParents
 	 * 			Maximum parents identifier.
@@ -40,167 +39,123 @@ public class Parents {
 	 */
 	Parents(int maxParents){
 		this.parents = new Parent[maxParents];
-		this.counter = 0;
+		this.next = 0;
 	}
 	
 	/**
-	 * Method of the class. Add parents, checks the state of the array that stores them and checks if it is full, empty or if there has been an error when adding them.
+	 * Method of the class. Add a parent to the list and checks the state of the array that stores them and checks if it is full or if there has been an error when adding them.
 	 * 
 	 * @param parent
-	 * 			Identifier of the parent.
-	 * @return if it has been added correctly.
+	 * 			Identifier of the parent
+	 * @return if it has been added correctly
 	 * @throws BlablakidException
 	 * 			Exception of the class
 	 * 
 	 */
 	
 	public boolean addParent(Parent parent) throws BlablakidException {
-		log.entry();
+		//log.entry();
 		
-		boolean ok = false;
-		
-		if(this.isFull()) {
-			log.error("Parents error: No more room left.");
-			throw new BlablakidException("Parents error: No more room left.");
-		
-		}else if(this.checkParent(parent.getName() != -1)) {
-			log.error("Add parent error: Parent " + parent.getName() + " already in the aplication.");
-			throw new BlablakidException("Add parent error: Parent " + parent.getName() + " already in the aplication.");
-		
-		}else if(parent.getName().isEmpty()) {
-			log.error("Add parent error: Parent's name is empty.");
-			throw new BlablakidException("Add parent error: Parent's name is empty.");
-		
-		}else {
-			parents[this.counter] = parent;
-			ok = true;
-			this.counter++;
+		if(search(parent.getName()) == true){
+			//log.error("Add parent error: Parent " + parent.getName() + " already in the aplication.");
+			throw new BlablakidException("Add Parent error: Parent " + parent.getName() + " already in the aplication.");
 		}
-		log.exit();
+		else if(this.next == parents.length){
+			//log.error("Add parent error: The list is full");
+			throw new BlablakidException("Add parent error: The list is full");
+		}
+		else{
+			this.parents[this.next++] = parent;
+			return true;
+		}
+		//log.exit();
+	}
+	
+	/**
+	 * Method of the class. Search a parent by its name.
+	 * @param name
+	 * @return boolean
+	 */
+	public boolean search(String name){
+		boolean ok = false;
+		int counter = 0;
+		while(counter < parents.length){
+			if(isSame(this.parents[counter].getName(), name)){
+				ok = true;
+				break;
+			}else{
+				ok = false;
+			}
+		}
+		
 		return ok;
 	}
 	
 	/**
-	 * Method of the class. Restarts the counter.
-	 * 
-	 */
-	public void restart() {
-		this.counter = 0;
-	}
-	
-	/**
-	 * Method of the class. Checks if the array that stores the parents is full.
-	 * 
-	 * @return if the array is full returns true and if it's not returns false.
-	 * 
-	 */
-	public boolean isFull() {
-		if(counter == parents.length) return true;
-		else return false;
-	}
-	
-	/**
-	 * Method of the class. Returns the length of the array that stores the parents.
-	 * 
-	 * @return length of the array.
-	 * 
-	 */
-	public int length() {
-		return this.counter;
-	}
-	
-	/**
-	 * Method of the class. Checks if the parent  is repeated.
-	 * 
-	 * @param parent
-	 * 			Identifier of the parent.
-	 * @return the position.
-	 * 
-	 */
-	public int checkParent(String parent) {
-		int position = binarySearch(new Parent(parent, 0));
-		return position;
-	}
-	
-	/**
-	 * Method of the class. Returns, after a comparison, the position where the first free hole is located, within the array of parents.
-	 * 
-	 * @param parent
-	 * 			Identifier of the parent.
-	 * @return the value that will be passed to the method.
-	 * 
-	 */
-	public int binarySearch(Parent parent) {
-		int value = -1;
-		for(int i = 0; i < counter; i++) {
-			if(parent.getName().compareTo(parents[i].getName()) == 0) {
-				value = i;
-				i = counter;
-			}
-		}
-		return value;
-	}
-	
-	/**
-	 * Method of the class. Returns the parent.
-	 * 
-	 * @param i
-	 * 			Parameter that serves to return.
-	 * @return	the parent who is in the position i of the parents' array.
-	 * 
-	 */
-	public Parent getParent(int i) {
-		return parents[i];
-	}
-	
-	/**
-	 * Method of the class. Returns the parent's name.
-	 * 
+	 * Method of the class. Compare two parents to know if are the same.
 	 * @param name
-	 * 			Identifier of the parent's name.
-	 * @return the parent's name.
-	 * 
+	 * @param newName
+	 * @return boolean
 	 */
-	public Parent getParent(String name) {
-		return parents[checkParent(name)];
+	public boolean isSame(String name, String newName){
+		if(name.equals(newName)){
+			return true;
+		}else{
+			return false;
+		}
 	}
 	
 	/**
 	 * Method of the class. Remove a parent from the array of parents.
 	 * 
 	 * @param parent
-	 * 			Identifier of the parent's name.
-	 * @throws Blablakidexception
-	 * 			Exception of the class.
-	 * 
+	 * 			Identifier of the parents
+	 * @throws BlablakidException 
+	 * 			Exception of the class
+	 *
 	 */
-	public void remove(Parent parent) throws BlablakidException {
+	public void removeKid(Parent parent) throws BlablakidException {
 		int pos = this.checkParent(parent.getName());
 		if(pos == -1) {
 			throw new BlablakidException("Error, this parent it is not in the aplication.");
 			
 		}else {
 			parents[pos] = null;
-			move(parents);
-			counter--;
+			move();
+			this.next--;
 		}
 	}
 	
 	/**
 	 * Method of the class. Moves a position parent, and places it where there is a free space.
 	 * 
-	 * @param parents
-	 * 			Identifier of the parents array.
-	 * 
 	 */
-	public void move(Parent[] parents) {
-		for (int i = 0; i < counter; i++) {
-			if(parents[i] == null && i != parents.length - 1) {
-				parents[i] = parents[(i+1)];
-				parents[(i+1)] = null;
+	private void move() {
+		for(int i = 0; i < parents.length-1; i++) {
+			if(parents[i] == null) {
+				parents[i] = parents[i+1];
+				parents[i+1] = null;
 			}
 		}
 	}
+	
+	/**
+	 * Method of the class. Checks if the parent is repeated.
+	 * 
+	 * @param name
+	 * @return the position.
+	 * 
+	 */
+	public int checkParent(String name) {
+		for(int i = 0; i < parents.length; i++) {
+			if(parents[i].getName().equals(name)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	
 	
 	/**
 	 * Method of the class. Prints the content of the parents array.
@@ -208,71 +163,13 @@ public class Parents {
 	 * @return the content of the parents array.
 	 * 
 	 */
-	public String toString() {
+	/*public String toString() {
 		StringBuilder output = new StringBuilder();
 		
 		for(int i = 0; i < this.counter; i++) {
 			output.append(parents[i].toString() + "\n");
 		}
 		return output.toString();
-	}
-	
-	
-	
-	/*private String padre;
-	private int hijos;
-	private int rides;
-	private String fav;
-	
-	public parents() {
-		
-		padre="desconocido";
-		hijos=0;
-		rides=0;
-		fav="desconocido";
-	}
-	
-	public void setNom(String Pnom) {
-		
-		this.padre=padre;
-	}
-	
-	public String getNom() {
-		
-		return padre;
-	}
-	
-	public void setHijos(int hijos) {
-		
-		this.hijos=hijos;
-	}
-	
-	public int getHijos() {
-		
-		return hijos;
-		
-	}
-
-	public void setRides(int rides) {
-		
-		this.rides=rides;
-	}
-	
-	public int getRides() {
-		
-		return rides;
-		
-	}
-	
-	public void setFav(String fav) {
-		
-		this.fav=fav;
-		
-	}
-	
-	public String getFav() {
-		
-		return fav;
-		
 	}*/
+	
 }
