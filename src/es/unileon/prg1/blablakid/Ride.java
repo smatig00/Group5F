51fddcf3948d1ad6f8841956298a49753de33629
smatig00 <36730109+smatig00@users.noreply.github.com>
2019-05-16 07:48:46 +0200

    Update Ride.java
    clase sin errores

--- src/es/unileon/prg1/blablakid/Ride.java
+++ src/es/unileon/prg1/blablakid/Ride.java
@@ -10,34 +10,101 @@ package es.unileon.prg1.blablakid;
 
 public class Ride {
 	
-	private String Sride;
-	private String endRide;
+	private String name;
+	private Parent parentName;
+	private Kid kidName;
+	private Time startRide;
+	private Time endRide;
+	private String endPlace;
+	private String startPlace;
 	
-	public Ride() {
+	
+	public Ride(String name, Parent parentName, Kid kidName, Time startRide, Time endRide, String startPlace, String endPlace) {
 		
-		Sride="desconocido";
-		endRide="desconocido";
+		this.name = name;
+		this.parentName = parentName;
+		this.kidName = kidName;
+		this.startRide = startRide;
+		this.endRide = endRide;
+		this.startPlace = startPlace;
+		this.endPlace = endPlace;
 		
 	}
 	
-	public void setSride(String Sride) {
-		
-		this.Sride=Sride;
+	public void setName(String newName) {
+		this.name = newName;
 	}
 	
-	public String getSride() {
-		
-		return Sride;
+	public String getName() {
+		return name;
 	}
 	
-	public void setEndRide (String endRide) {
-		
-		this.endRide=endRide;
+	public void setParentName(Parent newParentName) {
+		this.parentName = newParentName;
 	}
 	
-	public String getEndRide() {
+	public Parent getParentName() {
+		return parentName;
+	}
+
+	public void setKidName(Kid newKidName) {
+		this.kidName = newKidName;
+	}
+	
+	public Kid getKidName() {
+		return kidName;
+	}
+	
+	public void setStartRide(Time newStartRide) {
+		
+		this.startRide = newStartRide;
+	}
+	
+	public Time getStartRide() {
+		
+		return startRide;
+	}
+	
+	public void setEndRide (Time newEndRide) {
+		
+		this.endRide = newEndRide;
+	}
+	
+	public Time getEndRide() {
 		
 		return endRide;
+	}
+	
+	public void setStartPlace(String newStartPlace) {
+		
+		this.startPlace = newStartPlace;
+	}
+	
+	public String getStartPlace() {
+		
+		return  startPlace;
+	}
+	
+	public void setEndPlace(String newEndPlace) {
+		
+		this.endPlace = newEndPlace;
+	}
+	
+	public String getEndPlace() {
+		
+		return endPlace;
+	}
+	
+	public String toString() {
+		String buffer = "";
+		buffer = buffer + name;
+		buffer = buffer + " " + parentName;
+		buffer = buffer + " " + kidName;
+		buffer = buffer + " " + startRide.toString();
+		buffer = buffer + " " + endRide.toString();
+		buffer = buffer + " " + startPlace;
+		buffer = buffer + " " + endPlace;
+		return buffer;
 	}
 
 }
