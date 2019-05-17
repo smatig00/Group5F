034dd556df127400345a437ba4ea190df3e55a37
smatig00 <36730109+smatig00@users.noreply.github.com>
2019-05-17 05:57:34 +0200

    Update KidTest.java

--- test/es/unileon/prg1/blablakid/KidTest.java
+++ test/es/unileon/prg1/blablakid/KidTest.java
@@ -1,10 +1,33 @@
 package es.unileon.prg1.blablakid;
 
+import static org.junit.Assert.*;
+import es.unileon.prg1.blablakid.BlablakidException;
+import es.unileon.prg1.blablakid.Kid;
+import org.junit.Before;
+import org.junit.Test;
+
 public class KidTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Kid Pablo;
+	
+	@Before
+		public void setUp()throws BlablakidException{
+		Pablo = new Kid("Pablo");
+	}
+	
+	@Test
+	public void testKid() throws BlablakidException{
+		Kid Rosa = new Kid(new Kid("Rosa"));
+		assertEquals("Rosa", Rosa.getName());
+	}
+	
+	@Test
+	public void testGetName() throws BlablakidException{
+		assertEquals("Pablo", Pablo.getName());
+	}
+	
+	@Test
+	public void testToString() throws BlablakidException{
+		assertEquals("Pablo", Pablo.toString());
 	}
 
 }
