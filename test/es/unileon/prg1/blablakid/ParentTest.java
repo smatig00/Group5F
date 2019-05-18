6d18399aabf1f78927e05e82801320ab54123bb8
smatig00 <36730109+smatig00@users.noreply.github.com>
2019-05-18 05:46:54 +0200

    Update ParentTest.java

--- test/es/unileon/prg1/blablakid/ParentTest.java
+++ test/es/unileon/prg1/blablakid/ParentTest.java
@@ -1,10 +1,43 @@
 package es.unileon.prg1.blablakid;
 
+import static org.junit.Assert.*;
+import es.unileon.prg1.blablakid.BlablakidException;
+import es.unileon.prg1.blablakid.Parent;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ParentTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Parent Pedro;
+	
+	@Before
+	public void setUp() throws BlablakidException {
+		Pedro = new Parent("Pedro", new Kids(3));
+		
+	}
+	
+	@Test 
+	public void testParent() throws BlablakidException {
+		Parent Ana = new Parent("Ana", new Kids(3));
+		assertEquals("Ana", Ana.getName());
+		
+	}
+	
+	@Test
+	public void testGetName() throws BlablakidException {
+		assertEquals("Pedro", Pedro.getName());
+		
+	}
+	
+	@Test
+	public void testGetKids() throws BlablakidException {
+		assertEquals(3, Pedro.getKids());
+		
+	}
+	
+	@Test
+	public void testToString() throws BlablakidException {
+		assertEquals("Pedro 3", Pedro.toString());
+		
 	}
 
 }
