2e33348a5046224835602fd6d9a8a427c3185867
smatig00 <36730109+smatig00@users.noreply.github.com>
2019-05-21 06:05:33 +0200

    Update WeekTest.java

--- test/es/unileon/prg1/blablakid/WeekTest.java
+++ test/es/unileon/prg1/blablakid/WeekTest.java
@@ -1,10 +1,13 @@
 package es.unileon.prg1.blablakid;
 
+import static org.junit.Assert.*;
+import es.unileon.prg1.blablakid.BlablakidException;
+import es.unileon.prg1.blablakid.Week;
+import org.junit.Before;
+import org.junit.Test;
+
 public class WeekTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
-
+	
 }
