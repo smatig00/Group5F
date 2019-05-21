910b226282cb9e0592a3beb16a1d2b202ddb413c
smatig00 <36730109+smatig00@users.noreply.github.com>
2019-05-21 05:58:06 +0200

    Update ParentsTest.java

--- test/es/unileon/prg1/blablakid/ParentsTest.java
+++ test/es/unileon/prg1/blablakid/ParentsTest.java
@@ -1,10 +1,76 @@
 package es.unileon.prg1.blablakid;
 
+import static org.junit.Assert.*;
+import es.unileon.prg1.blablakid.BlablakidException;
+import es.unileon.prg1.blablakid.Parents;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ParentsTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Parents parents;
+	
+	@Before
+	public void setUp() throws BlablakidException{
+		parents = new Parents(3);
+		parents.add(new Parent("Monica", new Kids(3)));
+	
+	}
+	
+	@Test
+	public void testAddOk() throws BlablakidException{
+		parents.add(new Parent("Lucas", new Kids(4)));
+		
+	}
+	
+	@Test(expected = BlablakidException.class)
+	public void testAddRepeated() throws BlablakidException{
+		parents.add(new Parent("Monica", new Kids(3)));
+	
+	}
+	
+	@Test(expected = BlablakidException.class)
+	public void testAddListFull() throws BlablakidException{
+		parents = new Parents(0);
+		parents.add(new Parent("Laura", new Kids(2)));
+		
+	}
+	
+	@Test
+	public void testSearchOk() throws BlablakidException{
+		assertTrue(new Parent("Monica", new Kids(3)).equals(parents.search("Monica")));
+	
+	}
+	
+	@Test
+	public void testSearchFail() throws BlablakidException{
+		assertEquals(null, parents.search("Monica"));
+		
+	}
+	
+	@Test
+	public void testIsSameTrue() throws BlablakidException{
+		assertEquals(true, parents.isSame("Monica", "Monica"));
+		
+	}
+	
+	@Test
+	public void testIsSameFalse() throws BlablakidException{
+		assertEquals(false, parents.isSame("Monica", "Lucas"));
+		
+	}
+	
+	@Test
+	public void testRemoveParentOk() throws BlablakidException{
+		parents.add(new Parent("Juan", new Kids(5)));
+		parents.removeParent("Monica");
+	
+	}
+	
+	@Test
+	public void testRemoveParentNoExists() throws BlablakidException{
+		parents.removeParent("Ana");
+		
 	}
 
 }
