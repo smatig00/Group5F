8166ddc425e49eae51bf740c4cd63ab5c54a2748
smatig00 <36730109+smatig00@users.noreply.github.com>
2019-05-21 06:04:23 +0200

    Update TimeTest.java

--- test/es/unileon/prg1/blablakid/TimeTest.java
+++ test/es/unileon/prg1/blablakid/TimeTest.java
@@ -1,10 +1,26 @@
 package es.unileon.prg1.blablakid;
 
+import static org.junit.Assert.*;
+import es.unileon.prg1.blablakid.BlablakidException;
+import es.unileon.prg1.blablakid.Time;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TimeTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	Time time;
+	
+	@Before
+	public void setUp() throws BlablakidException{
+		time = new Time(10, 30);
+		
 	}
+	
+	@Test
+	public void testToString() throws BlablakidException{
+		assertEquals("10:30", time.toString());
+	}
+
+	
 
 }
