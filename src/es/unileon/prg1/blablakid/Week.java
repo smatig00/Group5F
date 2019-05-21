4e5a75d0c1ed77ba607cda3172b72fff09e51d8d
smatig00 <36730109+smatig00@users.noreply.github.com>
2019-05-21 05:54:13 +0200

    Update Week.java

--- src/es/unileon/prg1/blablakid/Week.java
+++ src/es/unileon/prg1/blablakid/Week.java
@@ -11,9 +11,11 @@ package es.unileon.prg1.blablakid;
 public class Week {
 
 	private Day[] days;
+	private int next;
 	
 	public Week(int maxRides) {
 		this.days = new Day[5];
+		this.next = 0;
 		//TODO crear los dias de la semana
 		for(int i = 0; i < 5; i++) {
 			days[i] = 
@@ -21,27 +23,91 @@ public class Week {
 		}
 	}
 	
-	public void add(Day day) {
+	public boolean add(Day day) throws BlablakidException{
+		//log.entry();
 		
+		if(search(day.getDay()) == true){
+			//log.error("Add Day error: Kid " + day.getDay() + " already in the aplication.");
+			throw new BlablakidException("Add day error: Kid " + day.getDay() + " already in the aplication.");
+			//return false;
+		}
+		else if(this.next == days.length){
+			//log.error("Add day error: The list is full");
+			throw new BlablakidException("Add day error: The list is full");
+			//return false;
+		}
+		else{
+			this.days[this.next++] = day;
+			return true;
+		}
+		//log.exit();
 	}
 	
-	public void removeDay() {
+	public boolean search(Week day){
+		boolean ok = false;
+		int counter = 0;
+		while(counter < days.length){
+			if(isSame(this.days[counter].getDay(), day)){
+				ok = true;
+				break;
+			}else{
+				ok = false;
+			}
+		}
 		
+		return ok;
 	}
 	
-	public void search() {
-		
+	public boolean isSame(Week day, Week newDay){
+		if(day.equals(newDay)){
+			return true;
+		}else{
+			return false;
+		}
 	}
 	
-	public void move() {
-		
+	public void removeDay(String name) throws BlablakidException {
+		int pos = this.checkDay(name);
+		if(pos == -1) {
+			throw new BlablakidException("Error, this day it is not in the aplication.");
+			
+		}else {
+			days[pos] = null;
+			move();
+			this.next--;
+		}
+	}
+
+	
+	private void move() {
+		for(int i = 0; i < days.length-1; i++) {
+			if(days[i] == null) {
+				days[i] = days[i+1];
+				days[i+1] = null;
+			}
+		}
+	}
+	
+	public int checkDay(String name) {
+		for(int i = 0; i < days.length; i++) {
+			if(days[i].getName().equals(name)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 	public String toString() {
-		return null;
+		StringBuilder output = new StringBuilder();
+		
+		for(int i = 0; i < this.next; i++) {
+			output.append(days[i].toString() + "\n");
+		}
+		return output.toString();
 	}
 	
-//metodo añadir dia
+	
+	//metodo añadir dia
 	//remove dia
 	//buscar si ya esta el dia
 	//ordenar el array al eliminar un ride para tal dia
